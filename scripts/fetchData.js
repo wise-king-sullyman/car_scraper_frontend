23208ceed1cc50507199af9f48a98d22bcea36a4
Sully <wise.king.sullyman@gmail.com>
2023-01-29 14:30:43 -0500

    Refactor data fetching for clarity

--- scripts/fetchData.js
+++ scripts/fetchData.js
@@ -1,6 +1,30 @@
 import get from "axios";
 import { parse } from "node-html-parser";
 
+function extractDetailedInfo(detail) {
+  function getByQuerySelector(selector, type) {
+    const node = detail.querySelector(selector);
+
+    switch (type) {
+      case "distance":
+        return node?.textContent?.split(" ")[6];
+      case "link":
+        return 'https://cars.com' + node?.attrs.href;
+      default:
+        return node?.textContent;
+    }
+  }
+
+  const title = getByQuerySelector(".title");
+  const type = getByQuerySelector(".stock-type");
+  const mileage = getByQuerySelector(".mileage");
+  const price = getByQuerySelector(".primary-price");
+  const distance = getByQuerySelector(".miles-from", "distance");
+  const link = getByQuerySelector(".vehicle-card-link", "link");
+
+  return { title, type, mileage, price, distance, link };
+}
+
 async function fetchData(url) {
   const data = await get(url).then((res) => res.data);
 
@@ -8,26 +32,7 @@ async function fetchData(url) {
 
   const details = root.querySelectorAll(".vehicle-details");
 
-  const aggregated_info = details.map((detail) => {
-    function getContentByQuerySelector(selector) {
-      const content = detail.querySelector(selector)?.textContent;
-      if (content) {
-        return content;
-      } else {
-        console.warn(`no text found for selector ${selector}`);
-      }
-    }
-
-    const title = getContentByQuerySelector(".title");
-    const type = getContentByQuerySelector(".stock-type");
-    const mileage = getContentByQuerySelector(".mileage");
-    const price = getContentByQuerySelector(".primary-price");
-    const distance = getContentByQuerySelector(".miles-from").split(" ")[6];
-    const link =
-      "https://www.cars.com" +
-      detail.querySelector(".vehicle-card-link").attrs.href;
-    return { title, type, mileage, price, distance, link };
-  });
+  const aggregated_info = details.map((detail) => extractDetailedInfo(detail));
 
   return aggregated_info;
 }
