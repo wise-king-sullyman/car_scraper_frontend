bbeeb59a2b1deb504930c7a418ae409b9c379ebc
Sully <wise.king.sullyman@gmail.com>
2023-01-29 11:42:17 -0500

    init

---
+++ scripts/fetchData.js
@@ -0,0 +1,68 @@
+import get from 'axios'
+
+// const options = { mode: 'same-origin' as RequestMode } // no-cors, *cors, same-origin
+
+const urlBase = 'https://www.cars.com/shopping/results/?'
+// dealer_id=
+// &keyword=
+// &list_price_max=
+// &list_price_min=
+// &makes[]=volvo
+// &maximum_distance=all
+// &mileage_max=
+// &models[]=volvo-s60&models[]=volvo-s60_hybrid&models[]=volvo-s60_inscription&models[]=volvo-s60_recharge_plug_in_hybrid
+// &page_size=20
+// &sort=list_price
+// &stock_type=all
+// &trims[]=volvo-s60-t8_inscription
+// &trims[]=volvo-s60_recharge_plug_in_hybrid-t8_black_edition_r_design_extended_range
+// &trims[]=volvo-s60_recharge_plug_in_hybrid-t8_inscription_extended_range
+// &trims[]=volvo-s60_recharge_plug_in_hybrid-t8_plus_black_edition
+// &trims[]=volvo-s60_recharge_plug_in_hybrid-t8_plus_dark_theme
+// &trims[]=volvo-s60_recharge_plug_in_hybrid-t8_r_design_extended_range
+// &trims[]=volvo-s60_recharge_plug_in_hybrid-t8_ultimate_black_edition
+// &trims[]=volvo-s60_recharge_plug_in_hybrid-t8_ultimate_bright_theme
+// &year_max=
+// &year_min=2022
+// &zip=27599'
+const params = {
+dealer_id: '',
+keyword: '',
+list_price_max: '',
+list_price_min: '',
+makes: ['volvo'],
+maximum_distance: 'all',
+mileage_max: '',
+models: ['volvo-s60', 'volvo-s60_hybrid', 'volvo-s60_inscription', 'volvo-s60_recharge_plug_in_hybrid'],
+page_size: 200,
+sort: 'list_price',
+stock_type: 'all',
+trims: ['volvo-s60-t8_inscription', 'volvo-s60_recharge_plug_in_hybrid-t8_black_edition_r_design_extended_range',
+'volvo-s60_recharge_plug_in_hybrid-t8_inscription_extended_range', 'volvo-s60_recharge_plug_in_hybrid-t8_plus_black_edition',
+'volvo-s60_recharge_plug_in_hybrid-t8_plus_dark_theme', 'volvo-s60_recharge_plug_in_hybrid-t8_r_design_extended_range',
+'volvo-s60_recharge_plug_in_hybrid-t8_ultimate_black_edition', 'volvo-s60_recharge_plug_in_hybrid-t8_ultimate_bright_theme'],
+year_max: '',
+year_min: 2022,
+zip: 27599
+}
+
+function buildUrl(base, params) {
+  const fullParams = Object.keys(params).reduce((acc, param) => {
+    const value = params[param];
+    if (typeof value === 'object') {
+      return value.reduce((acc, subParam) => `${acc}&${param}[]=${subParam}`, '')
+    } else {
+      return `${acc}&${param}=${value}`
+    }
+  }, '')
+  return base + fullParams.slice(1)
+}
+
+
+async function fetchData(url) {
+  console.log('fetching data')
+  return get(url).then(res => res.data)
+}
+
+// fetchData(url).then(res => console.log(res))
+console.log(buildUrl(urlBase, params))
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import fetchData from '../scripts/fetchData'
+
+const url = 'https://www.cars.com/shopping/results/?dealer_id=&keyword=&list_price_max=&list_price_min=&makes[]=volvo&maximum_distance=all&mileage_max=&models[]=volvo-s60&models[]=volvo-s60_hybrid&models[]=volvo-s60_inscription&models[]=volvo-s60_recharge_plug_in_hybrid&page_size=20&sort=list_price&stock_type=all&trims[]=volvo-s60-t8_inscription&trims[]=volvo-s60_recharge_plug_in_hybrid-t8_black_edition_r_design_extended_range&trims[]=volvo-s60_recharge_plug_in_hybrid-t8_inscription_extended_range&trims[]=volvo-s60_recharge_plug_in_hybrid-t8_plus_black_edition&trims[]=volvo-s60_recharge_plug_in_hybrid-t8_plus_dark_theme&trims[]=volvo-s60_recharge_plug_in_hybrid-t8_r_design_extended_range&trims[]=volvo-s60_recharge_plug_in_hybrid-t8_ultimate_black_edition&trims[]=volvo-s60_recharge_plug_in_hybrid-t8_ultimate_bright_theme&year_max=&year_min=2022&zip=27599'
 
 function App() {
+  const [data, setData] = React.useState('');
+
+  React.useEffect(() => {
+    fetchData(url).then(res => console.log(res))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data}
     </div>
   );
 }
